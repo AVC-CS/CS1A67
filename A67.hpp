81cbd66083ef20f73a8b2e53df4923870e2ff988
Kyu Lee <kwooler@gmail.com>
2022-12-13 12:04:20 -0800

    A67

--- A67.hpp
+++ A67.hpp
@@ -4,6 +4,8 @@
 #include <iostream>
 using namespace std;
 
-// complete the Overloading function swap( )
+// complete the Overloading function swap( ) and getinput( )
+// based on the main function.
+
 // swap ( ) function has the difference number of parameters from 2 to 4
 // For example, swap(10, 20); swap(10, 20, 30); swap(10, 20, 30, 40)
